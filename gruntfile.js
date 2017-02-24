9c0bd6102c6bdb83f67f0a2391c28bf6b5f649de
Tristan Deloris <borntowebb@gmail.com>
2017-02-24 23:35:24 +0100

    First Commit : created files

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    
+    grunt.initConfig({
+        
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+               files: [{
+                   expand: true,
+                    cwd: 'scss',
+                    src: ['*.scss'],
+                    dest: 'css/',
+                    ext: '.css'
+      }]
+            }
+        },
+        
+        watch: {
+            styles: {
+                files: 'scss/*.scss',
+                tasks: ['sass']
+            }
+        }
+        
+    })
+    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('default',['sass'],'watch')
+    
+}
