742a54ff7460d351494b8a947de41c0fe8ee764d
tdeloris <tdeloris@companeo.com>
2017-05-02 18:43:26 +0200

    fixed the overflow hidden on body while clicking on the navtoggle

--- js/main.js
+++ js/main.js
@@ -2,18 +2,10 @@ var body = document.querySelector('body');
 var navToggle = document.querySelectorAll('.navToggle');
 var inputNav = document.querySelector('#navToggle');
 
-navToggle.addEventListener('click', function(){
-	for (inputNav = 0; index < inputNav.length; ++inputNav){
-		if(inputNav.checked === true){
-			body.classList.add('modal-open');
-		} else {
-			body.classList.remove('modal-open');
-		}
-	}
+inputNav.addEventListener('click', function () {
+    if (this.checked === true) {
+        body.classList.add('modal-open');
+    } else {
+        body.classList.remove('modal-open');
+    }
 });
-
-document.getElementById( 'setall' ).addEventListener( 'click', function(){
-  for (index = 0; index < checkboxlist.length; ++index) {
-    checkboxlist[index].setAttribute('checked', 'checked');
-  }
-});q
